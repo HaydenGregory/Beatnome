2000b1be9ebb89c126000ea092fc5622ccb3b94c
Hayden Gregory <gregory00123@gmail.com>
2021-09-02 15:53:17 -0500

    added copyright

--- src/App.js
+++ src/App.js
@@ -22,98 +22,109 @@ function App() {
     dragControls.start(e)
   }
   const color = useSelector(state => state.metronome.color[0])
-  console.log(color)
   return (
     <Router>
-      <AppDiv className="App">
-        <Switch>
-          <Route exact path='/'>
-            <div
-              ref={constraintsRef}
-              className={`contain ${color}`}>
-              <motion.div
-                style={{
-                  position: 'relative'
-                }}
-                drag
-                dragControls={dragControls}
-                dragListener={false}
-                dragConstraints={constraintsRef}>
+      <AppDiv className={`App`}>
+        <div className={color}>
+          <Switch>
+            <Route exact path='/'>
+              <div
+                ref={constraintsRef}
+                className={`contain `}>
                 <motion.div
                   style={{
-                    position: 'absolute',
-                    zIndex: 2,
-                    right: 6
+                    position: 'relative'
                   }}
-                  onPointerDown={startDrag}>🖐🏻</motion.div>
-                <Metronome />
-              </motion.div>
-              {animationToggle ? <Animation tempo={bpm} constraintsRef={constraintsRef} /> : ''}
-            </div>
-          </Route>
-          <Route exact path='/drums'>
-            <Video />
-            <div
-              ref={constraintsRef}
-              className={`contain ${color}`}>
-              <motion.div
-                style={{
-                  position: 'relative',
-                }}
-                drag
-                layout
-                dragControls={dragControls}
-                dragListener={false}
-                dragConstraints={constraintsRef}>
+                  drag
+                  dragControls={dragControls}
+                  dragListener={false}
+                  dragConstraints={constraintsRef}>
+                  <motion.div
+                    style={{
+                      position: 'absolute',
+                      zIndex: 2,
+                      right: 6
+                    }}
+                    onPointerDown={startDrag}>🖐🏻</motion.div>
+                  <Metronome />
+                </motion.div>
+                {animationToggle ? <Animation tempo={bpm} constraintsRef={constraintsRef} /> : ''}
+                <div className={`copyright`}>
+                  <span>© 2021 Hayden Gregory All Rights Reserved. || | </span>
+                  <span>| Designed by Hayden Gregory</span>
+                </div>
+              </div>
+            </Route>
+            <Route exact path='/drums'>
+              <Video />
+              <div
+                ref={constraintsRef}
+                className={`contain ${color}`}>
                 <motion.div
                   style={{
-                    position: 'absolute',
-                    zIndex: 2,
-                    right: 6
+                    position: 'relative',
                   }}
-                  onPointerDown={startDrag}>🖐🏻</motion.div>
-                <Drums />
-              </motion.div>
-            </div>
-          </Route>
-          <Route exact path='/ye'>
-            <Video />
-            <div
-              ref={constraintsRef}
-              className={`contain ${color}`}>
-              <motion.div
-                style={{
-                  position: 'relative',
-                }}
-                drag
-                layout
-                dragControls={dragControls}
-                dragListener={false}
-                dragConstraints={constraintsRef}>
+                  drag
+                  layout
+                  dragControls={dragControls}
+                  dragListener={false}
+                  dragConstraints={constraintsRef}>
+                  <motion.div
+                    style={{
+                      position: 'absolute',
+                      zIndex: 2,
+                      right: 6
+                    }}
+                    onPointerDown={startDrag}>🖐🏻</motion.div>
+                  <Drums />
+                </motion.div>
+                <div className={`copyright`}>
+                  <span>© 2021 Hayden Gregory All Rights Reserved. || | </span>
+                  <span>| Designed by Hayden Gregory</span>
+                </div>
+              </div>
+            </Route>
+            <Route exact path='/ye'>
+              <Video />
+              <div
+                ref={constraintsRef}
+                className={`contain ${color}`}>
                 <motion.div
                   style={{
-                    position: 'absolute',
-                    zIndex: 2,
-                    right: 6
+                    position: 'relative',
                   }}
-                  onPointerDown={startDrag}>🖐🏻</motion.div>
-                <About />
-              </motion.div>
-            </div>
-          </Route>
-        </Switch>
-        <div className="copyright">
-          <span>© 2021 Hayden Gregory All Rights Reserved. || | </span>
-          <span>| Designed by Hayden Gregory</span>
+                  drag
+                  layout
+                  dragControls={dragControls}
+                  dragListener={false}
+                  dragConstraints={constraintsRef}>
+                  <motion.div
+                    style={{
+                      position: 'absolute',
+                      zIndex: 2,
+                      right: 6
+                    }}
+                    onPointerDown={startDrag}>🖐🏻</motion.div>
+                  <About />
+                </motion.div>
+                <div className={`copyright`}>
+                  <span>© 2021 Hayden Gregory All Rights Reserved. || | </span>
+                  <span>| Designed by Hayden Gregory</span>
+                </div>
+              </div>
+            </Route>
+          </Switch>
         </div>
       </AppDiv>
     </Router>
   );
 }
 const AppDiv = styled.div`
+
       display: flex;
       flex-direction: column;
-.contain{
+
+  .contain{
       display: flex;
       flex-direction: column;
       justify-content:center;
@@ -121,22 +132,29 @@ const AppDiv = styled.div`
       width: 100vw;
       height: 100vh
   }
+
   .copyright{
     display: flex;
+    position:absolute;
+    bottom: 0;
+    right: 0;
+    justify-content: flex-end;
     font-size:10px;
+    border: none;
+    border-style:none;
   }
-      .pink{
-        background-color: pink;
+  .pink{
+    background-color: pink;
   }
-      .yellowgreen{
-        background-color: yellowgreen;
+  .yellowgreen{
+    background-color: yellowgreen;
   }
-      .green{
-        background-color: green;
+  .green{
+    background-color: green;
   }
-      .tomato{
-        background-color: tomato;
+  .tomato{
+    background-color: tomato;
   }
-      `
+`
 
 export default App;
